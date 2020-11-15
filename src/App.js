eab1f9f55610cd6377fa060af032a594fcd42f72
Dammirbuh <walidakka53@gmail.com>
2020-11-15 17:21:44 +0100

    edited

--- src/App.js
+++ src/App.js
@@ -1,56 +1,70 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Units from './Units.js';
+
+function getUnit(name, slec){
+  return Units[slec].filter((e) => {return e.name === name})[0];
+}
+
+function convertTo(name, unit, toName, selc){
+  const [stunit, ndunit] = [getUnit(name, selc), getUnit(toName, selc)];
+  return ((unit*stunit.to_meters)/ndunit.to_meters);
+
+}
 
 function App() {
-  const [date, setDate] = useState(null);
+  const [selected, setSelected]= useState("length");
+  const [stselected, setStSelected] = useState("foot");
+  const [ndselected, setNdSelected] = useState("foot");
+  const [stchoice, setStChoice] = useState(1);
+  const [ndchoice, setNdChoice] = useState(convertTo(stselected, stchoice, ndselected, selected));
+
   useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+    setStChoice(convertTo(ndselected, ndchoice, stselected, selected))
+  }, [stselected])
+  useEffect(() => {
+    setNdChoice(convertTo(stselected, stchoice, ndselected, selected))
+  }, [ndselected])
+
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
+    <div className="App">
+      <h1>Units Converter</h1>
+      <select className="Choices" onChange={(e) => {setSelected(e.target.value)}}>
+        {Object.keys(Units).map((u) => {
+          return <option value={u}>{u[0].toUpperCase()}{u.slice(1)}</option>
+        })}
+      </select>
+      <div className="Converts">
+        <div className="Choice">
+          <input type="number" onChange={(e) => {
+            setStChoice(Number(e.target.value));
+            setNdChoice(convertTo(stselected, e.target.value, ndselected, selected))
+          }} value={stchoice}/>
+          <br/>
+          <select className="Choices" onChange={(e) => {
+            setStSelected(e.target.value)
+          }}>
+            {Units[selected].map((unit) => {
+              return (<option value={unit.name}>{`${unit.name} (${unit.unit})`}</option>)
+            })}
+          </select>
+        </div>
+        <div className="Choice">
+          <input type="number" onChange={(e) => {
+            setNdChoice(Number(e.target.value)); 
+            setStChoice(convertTo(ndselected, e.target.value, stselected, selected))
+          }} value={ndchoice}/>
+          <br/>
+          <select className="Choices" onChange={(e) => {
+            setNdSelected(e.target.value)
+          }}>
+            {Units[selected].map((unit) => {
+              return (<option value={unit.name}>{`${unit.name} (${unit.unit})`}</option>)
+            })}
+          </select>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/Units.js
@@ -0,0 +1,45 @@
+const Units = {
+  "length": [
+    {
+      "name": "foot",
+      "unit": "ft",
+      "to_meters": 0.3048
+    },
+    {
+      "name": "meter",
+      "unit": "m",
+      "to_meters": 1
+    },
+    {
+      "name": "inch",
+      "unit": "in",
+      "to_meters": 0.0254
+    },
+    {
+      "name": "yard",
+      "unit": "yd",
+      "to_meters": 0.9144
+    },
+    {
+      "name": "mile",
+      "unit": "mi",
+      "to_meters": 1609.344
+    },
+    {
+      "name": "kilometer",
+      "unit": "km",
+      "to_meters": 1000
+    },
+    {
+      "name": "centimeter",
+      "unit": "cm",
+      "to_meters": 0.01
+    },
+    {
+      "name": "millimeter",
+      "unit": "mm",
+      "to_meters": 0.001
+    }
+  ]
+}
+export default Units;
